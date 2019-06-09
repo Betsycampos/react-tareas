b34694b888fb7c10ec63b2cc37f6728b2e57196f
Betsycampos <cbetsydalia@gmail.com>
2019-06-08 23:44:35 -0500

    Añadiendo el primer componente al archivo, navegacion.js ya tiene su primer rol de función

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navegacion from './componentes/navegacion.js';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Navegacion />
+
         <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <p>
-          Bienvenida al mundo de react
-        </p>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/componentes/navegacion.js
@@ -0,0 +1,17 @@
+import React, { Component }from 'react';
+
+class Navegacion extends Component {
+    render() {
+      return (
+        <div className="App">
+    <nav className="navbar navbar-dark bg-dark">
+      <a href="" className="text-white">
+       Tareas
+      </a>
+    </nav>
+    </div>
+      )
+    }
+}
+
+export default Navegacion;
