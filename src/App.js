a5076271009a023657403ab81ff50b6c0609b96e
Betsycampos <cbetsydalia@gmail.com>
2019-06-09 23:31:51 -0500

    Añadiendo cambios de esatdo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navegacion from './componentes/navegacion.js';
+
+import { tareas } from './tareas.json';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tareas
+    }
+  }
   render() {
     return (
       <div className="App">
-       <Navegacion />
+       <nav className="navbar navbar-dark bg-dark">
+         <a href="" className="text-white">
+           Tareas
+         </a>
+       </nav>
+       
 
         <img src={logo} className="App-logo" alt="logo" />
     </div>
